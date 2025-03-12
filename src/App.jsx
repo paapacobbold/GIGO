3f4c061dee34c15a30ad9a6acba1bed78b2f0116
paapacobbold <paapacobbold1234@gmail.com>
2025-03-12 03:40:33 +0000

    package changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignupPage from "./pages/SignupPage/SignupPage";
 import ReportWaste from "./pages/ReportWaste/ReportWastePage";
-import Overview from "./pages/OverviewPage/OverviewPage"
+import Overview from "./pages/OverviewPage/OverviewPage";
 import { Routes, Route } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import ProfilePage from "./pages/Profile/ProfilePage";
 import Subscription from "./pages/Subscription/SubscriptionPage";
 import About from "./pages/About/AboutPage";
 
+library.add(fas);
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Subscription/>} />
-      {/* <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<Subscription />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/> */}
+      <Route path="/dashboard" element={<Dashboard/>}/>
     </Routes>
   );
 };
